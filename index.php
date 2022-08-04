50d27dd00fa621d8b16fccee82d7837883a52964
HarisMI40 <86542926+HarisMI40@users.noreply.github.com>
2022-08-04 08:25:48 +0700

    + edit & hapus

--- index.php
+++ index.php
@@ -3,5 +3,13 @@
  $query = $db->query('select * from siswa');
 
  while ($data = $query->fetch() ) :?>
-    <p><?= $data['nama'] ?> </p>
+    <p>
+        <a href="edit.php?id=<?= $data['id']; ?>">
+            <?= $data['nama'] ?>
+        </a>
+        
+        <a href="proses_hapus.php?id=<?= $data['id']; ?>" style="color:white;background:red;padding:2px;border-radius:3px">
+            Hapus
+        </a>
+    </p>
 <?php endwhile ?>
