5aade5c7a334c580db3c91147c05e2c0f68454bc
HarisMI40 <86542926+HarisMI40@users.noreply.github.com>
2022-09-02 08:12:56 +0700

    Update beranda.php

--- session2/beranda.php
+++ session2/beranda.php
@@ -4,4 +4,6 @@ session_start();
 if(!isset($_SESSION["username"])) { // jika ada session username
    header("Location:form.php");
 }
- echo "ini berandaa";
+?>
+<h1>Halaman Beranda </h1>
+<a href="hapus_session.php">Logout</a>
