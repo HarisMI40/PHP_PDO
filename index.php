b39702e939873284acf865be5293703b058c10ab
HarisMI40 <86542926+HarisMI40@users.noreply.github.com>
2022-11-16 10:15:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-<?php
- $db = new PDO("mysql:host=localhost;dbname=sekolah__php_pdo",'root','');
- $query = $db->query('select * from siswa');
-
- while ($data = $query->fetch() ) :?>
-    <p>
-        <a href="edit.php?id=<?= $data['id']; ?>">
-            <?= $data['nama'] ?>
-        </a>
-        
-        <a href="proses_hapus.php?id=<?= $data['id']; ?>" style="color:white;background:red;padding:2px;border-radius:3px">
-            Hapus
-        </a>
-    </p>
-<?php endwhile ?>
+<h1>Heloooo</h1>
