0a76353d5846919d5217ff885718bd76470b0b2a
HarisMI40 <86542926+HarisMI40@users.noreply.github.com>
2022-11-16 10:34:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<h1>Heloooo</h1>
+<?php
+ $db = new PDO("mysql:host=containers-us-west-120.railway.app:7333;dbname=railway",'UDqUH6Jl3yPws7j2mslw','');
+ $query = $db->query('select * from siswa');
+
+ while ($data = $query->fetch() ) :?>
+    <p>
+        <a href="edit.php?id=<?= $data['id']; ?>">
+            <?= $data['nama'] ?>
+        </a>
+        
+        <a href="proses_hapus.php?id=<?= $data['id']; ?>" style="color:white;background:red;padding:2px;border-radius:3px">
+            Hapus
+        </a>
+    </p>
+<?php endwhile ?>
